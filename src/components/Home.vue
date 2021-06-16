adc87551765495a97b744d7cb44e602f2b733da3
radoRandriamahandry <61495462+radoRandriamahandry@users.noreply.github.com>
2021-06-16 13:25:37 +0300

    feat: add loading UI for the map section

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,31 @@
 <template>
   <div class="flex flex-col h-screen text-gray-700 bg-gray-100">
     <TheHeader />
-    <!-- TODO: create a loading spinner -->
 
-    <div v-if="getIpDetailsIsLoading" class="flex-1">
-      Waiting for IP position ...
-    </div>
-    <Map v-else class="flex-1" />
+    <transition
+      mode="out-in"
+      enter-from-class="opacity-0"
+      enter-active-class="transition-opacity duration-150 ease-out"
+      leave-to-class="opacity-0"
+      leave-active-class="transition-opacity duration-150 ease-in"
+    >
+      <DotLoader v-if="getIpDetailsIsLoading" class="flex-1" />
+      <Map v-else class="flex-1" />
+    </transition>
   </div>
 </template>
 
 <script>
 import TheHeader from "./TheHeader.vue"
 import Map from "./Map.vue"
+import DotLoader from "./ui/DotLoader.vue"
 import getIpDetailsStatus from "../store/getIpDetailsStatus"
 
 export default {
   components: {
     TheHeader,
     Map,
+    DotLoader,
   },
   setup() {
     const { getIpDetailsIsLoading } = getIpDetailsStatus()
---
+++ src/components/ui/DotLoader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="grid place-items-center">
+    <div class="flex items-baseline gap-2 dot-loader">
+      <span class="text-lg font-semibold text-gray-400">Waiting for data</span>
+      <span class="w-1 h-1 bg-gray-400 rounded-full animate-bounce"></span>
+      <span class="w-1 h-1 bg-gray-400 rounded-full animate-bounce"></span>
+      <span class="w-1 h-1 bg-gray-400 rounded-full animate-bounce"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.dot-loader span:nth-child(3) {
+  animation-delay: 0.2s;
+}
+.dot-loader span:nth-child(4) {
+  animation-delay: 0.4s;
+}
+</style>
